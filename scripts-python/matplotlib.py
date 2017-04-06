19c79fe135fe5993ca62c998f2d725301194c095
Daniela Onita <danielaonita25@gmail.com>
2017-04-06 22:22:39 +0300

    Matplotlib plotting from a CSV

--- scripts-python/matplotlib.py
+++ scripts-python/matplotlib.py
@@ -7,9 +7,9 @@ Created on Thu Apr  6 15:17:20 2017
 
 from matplotlib import pyplot as plt
 from matplotlib import style
-
+import numpy as np
 #style.use('ggplot')
-
+'''
 x = [5,6,7,8]
 y = [7,3,8,3]
 
@@ -19,11 +19,17 @@ print(len(x))
 print(len(y))
 
 plt.bar(x,y, color='c')
-
 plt.bar(x2,y2, color='g')
+'''
 
+#load file with numpy lib
+
+x,y = np.loadtxt('exempleFile.txt', unpack=True, delimiter=',')
+print(x)
+print(y)
+#plot with x, y from file
+plt.plot(x,y)
 plt.title('chart')
-
 plt.ylabel('Y axis')
 plt.xlabel('X axis')
 
